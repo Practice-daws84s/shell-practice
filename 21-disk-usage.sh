b43185d63bad0b43da2da8af3b9acc9ed965bf86
suresh228devops <suresh228devops@gmail.com>
2026-01-19 04:54:35 +0530

    Shell

---
+++ 21-disk-usage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=1 # In project it will be 75
+MSG=""
+
+while IFS = read line
+do
+    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%d" -f1)
+    PARTITION=$(echo $line | awk '{print $7F}')
+    echo "$$PARTITION: $USAGE"
+    if [ $USAGE -ge $DISK_THRESHOLD]
+    then
+        MSG+="High Disk Usage on $PARTITION: $USAGE"
+        echo $MSG
+    fi
+done <<< $DISK_USAGE
